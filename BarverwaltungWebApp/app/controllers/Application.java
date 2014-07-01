a3466923b596090340cbf128b0f1c4b7eb70bc3a
Andreas Wittmann <e1225854@student.tuwien.ac.at>
2014-07-01 14:33:59 +0200

    Navigation implementiert

--- BarverwaltungWebApp/app/controllers/Application.java
+++ BarverwaltungWebApp/app/controllers/Application.java
@@ -8,7 +8,29 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render());
     }
+    
+    public static Result getSalesOverview()
+    {
+    	return ok(salesOverview.render());
+    }
+    
+    public static Result getAccountOverview()
+    {
+    	return ok(accountOverview.render());
+    }
+    
+    public static Result getPurchaseOverview()
+    {
+    	return ok(purchaseOverview.render());
+    }
+    
+    public static Result getStockOverview()
+    {
+    	return ok(stockOverview.render());
+    }
+    
+    
 
 }
